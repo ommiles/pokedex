4ce71711b6219ad1b5656df354bce3e0ef863e85
OM Miles <olivia.m.miles@gmail.com>
2021-03-10 20:25:35 -0800

    edits

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-const url = `https://pokeapi.co/api/v2/pokemon/mew/`
-
-console.log(document.querySelector(`.form-search`))
+document.querySelector(`.form-search`).addEventListener(`submit`,(e) => { 
+    e.preventDefault()
+    console.log(e.target.search.value)
+    fetchPokemon(e.target.search.value)
+})
 
 // fetch request goes to URL
 // makes a request for data from pokemon api
-fetch(url)
-// data is retrieved from web api
-// data is translated
-.then(res => res.json())
-// data is console logged
-.then(pokemon => console.log(pokemon))
+let fetchPokemon = pokemon => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+    // data is retrieved from web api
+    // data is translated
+    .then(res => res.json())
+    // data is console logged
+    .then(pokemon => buildCard(pokemon))
+}
 
+let buildCard = pokemon => {
+    // grab HTML els 
+    let name = document.querySelector(`#name`)
+    let img = document.querySelector(`#pokemon-img`)
 
+    // 
+    name.textContent = `${pokemon.name} : ${pokemon.id}`
+    img.src = pokemon.sprites.front_default
+}
